b54417ade8dd2cbf257d42797ac8006f10d3aad3
awongh <akira@awongh.com>
2016-11-03 22:28:04 +0700

    stuff and things

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var start = false;
-var startButton, audioCtx;
+var startButton, audioCtx, thresholdInput;
+var threshold = 160;
+//var currentColor = 0;
+//var colors = [];
 
 /////////
 
@@ -18,13 +21,6 @@ function visualize(stream) {
   draw()
 
   function draw(timestamp) {
-    if (!start) start = timestamp;
-    var progress = timestamp - start;
-
-    if (progress < 2000) {
-      //requestAnimationFrame(draw);
-    }
-
     requestAnimationFrame(draw);
 
     analyser.getByteFrequencyData(dataArray);
@@ -32,9 +28,11 @@ function visualize(stream) {
     for(var i = 0; i < 10; i++) {
  
       var node = document.querySelector('#n'+i);
-      var v = dataArray[(i+1)*8];
+      var x = (i+1) * 8;
+      var v = dataArray[x];
       //console.log( v );
-      if (v > 10){
+      alert(v );
+      if (v > threshold){
         //start animation on element
         node.style="background-color:#38ff88"
       } else {
@@ -56,40 +54,42 @@ function visualize(stream) {
 /////////
 
 window.onload = function(){
-  startButton = document.querySelector('#outer-cont');
+  //startButton = document.querySelector('#outer-cont');
 
-  startButton.onclick = function(){
+  thresholdInput = document.querySelector('#threshold');
+  thresholdInput.onchange = function(event){
+    threshold = event.target.value;
+  }
+
+  //startButton.onclick = function(){
+  //  currentColor++;
+  //};
+
+  // visualiser setup - create web audio api context
+  audioCtx = new (window.AudioContext)();
+
+  //main block for doing the audio recording
+  var onSuccess = function(stream) {
+    visualize(stream);
+  }
+
+  var onError = function(err) {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.getUserMedia = ( navigator.getUserMedia  )
+
+  if (navigator.getUserMedia) {
+    console.log('getUserMedia supported.');
+
+    var constraints = { audio: true };
+    var chunks = [];
+
+    navigator.getUserMedia(constraints, onSuccess, onError);
+  } else {
+     console.log('getUserMedia not supported on your browser!');
+  }
 
 
-    // visualiser setup - create web audio api context
-    audioCtx = new (window.AudioContext || webkitAudioContext)();
 
-    //main block for doing the audio recording
-    var onSuccess = function(stream) {
-      visualize(stream);
-    }
-
-    var onError = function(err) {
-      console.log('The following error occured: ' + err);
-    }
-
-    navigator.getUserMedia = ( navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia);
-
-    if (navigator.getUserMedia) {
-      console.log('getUserMedia supported.');
-
-      var constraints = { audio: true };
-      var chunks = [];
-
-      navigator.getUserMedia(constraints, onSuccess, onError);
-    } else {
-       console.log('getUserMedia not supported on your browser!');
-    }
-
-
-
-  };
 };
